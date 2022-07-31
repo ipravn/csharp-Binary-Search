0099e10b509f07e30491631428bf513594b8c2b0
Praveen Kumar <46933956+ipravn@users.noreply.github.com>
2022-07-31 18:01:09 +0530

    Create Floor-Sorted-Array.cs

---
+++ Floor-Sorted-Array.cs
@@ -0,0 +1,56 @@
+// Binary Search Algorithm Using C#
+// Praveen Kumar - https://github.com/ipravn/
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 8, 10, 10, 12, 19 };
+        int target = 15;
+        int result = BinarySearch(arr, target);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+
+    static int BinarySearch(int[] arr, int target)
+    {
+        int start = 0;
+        int end = arr.Length - 1;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] > target)
+            {
+                end = mid - 1;
+            }
+            else if (target > arr[mid])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                return start;
+            }
+        }
+        return arr[end] ;
+    }
+
+}
